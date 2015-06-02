216bd717d9b670acfa997b660a76d4692244cb6b
Luis <luis.panadero@gmail.com>
2015-06-02 19:41:21 +0200

    Update config.fish
    Added alias to open neovim on a konsole window

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,7 @@
 set -x GTEST_ROOT /usr/src/gtest/
 # private stuff
 set -x CHANGELOG_GITHUB_TOKEN (cat ~/.config/fish/CHANGELOG_GITHUB_TOKEN)
+
+# Alias                                                                                                           
+# Open neovim on a Konsole window                                                                                 
+alias kvim="konsole --profile nvim -e nvim" 
