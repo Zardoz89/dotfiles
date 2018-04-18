3f74f72c9279b8c8c816d9c649529e6a6db0b8fb
Luis Panadero Guardeño <luis.panadero@digibis.com>
2018-04-18 13:00:17 +0200

    Conditional includes for gitconfig

--- install.sh
+++ install.sh
@@ -8,26 +8,28 @@
 DIR=$(pwd)
 cd ~
 
-ln -s ${DIR}/bashrc ~/.bashrc
+ln -sf ${DIR}/bashrc ~/.bashrc
 
 ln -s ${DIR}/gitconfig ~/.gitconfig
+ln -sf ${DIR}/gitconfig-personal ~/.gitconfig-personal
+ln -sf ${DIR}/gitconfig-work ~/.gitconfig-work
 
-ln -s ${DIR}/config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ${DIR}/config/nvim/ginit.vim ~/.config/nvim/ginit.vim
-ln -s ${DIR}/config/nvim/init.vim ~/.vimrc
+ln -sf ${DIR}/config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ${DIR}/config/nvim/ginit.vim ~/.config/nvim/ginit.vim
+ln -sf ${DIR}/config/nvim/init.vim ~/.vimrc
 
-ln -s ${DIR}/config/fish/config.fish ~/.config/fish/config.fish
-ln -s ${DIR}/config/fish/fishfile ~/.config/fish/fishfile
+ln -sf ${DIR}/config/fish/config.fish ~/.config/fish/config.fish
+ln -sf ${DIR}/config/fish/fishfile ~/.config/fish/fishfile
 
-ln -s ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.kde/share/apps/konsole/WhiteOnBlack.colorscheme
-ln -s ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.kde/share/apps/konsole/Mio.colorscheme
-ln -s ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.kde/share/apps/konsole/Shell.profile
-ln -s ${DIR}/kde/share/apps/konsole/Fish.profile             ~/.kde/share/apps/konsole/Fish.profile
+ln -sf ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.kde/share/apps/konsole/WhiteOnBlack.colorscheme
+ln -sf ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.kde/share/apps/konsole/Mio.colorscheme
+ln -sf ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.kde/share/apps/konsole/Shell.profile
+ln -sf ${DIR}/kde/share/apps/konsole/Fish.profile             ~/.kde/share/apps/konsole/Fish.profile
 
 
-ln -s ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.local/share/konsole/WhiteOnBlack.colorscheme
-ln -s ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.local/share/konsole/Mio.colorscheme
-ln -s ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.local/share/konsole/Shell.profile
-ln -s ${DIR}/kde/share/apps/konsole/Fish.profile             ~/.local/share/konsole/Fish.profile
+ln -sf ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.local/share/konsole/WhiteOnBlack.colorscheme
+ln -sf ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.local/share/konsole/Mio.colorscheme
+ln -sf ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.local/share/konsole/Shell.profile
+ln -sf ${DIR}/kde/share/apps/konsole/Fish.profile             ~/.local/share/konsole/Fish.profile
 
 
