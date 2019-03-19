6dc0622b7756abda5a0d2a1fb26bc9775123ca38
Luis Panadero Guardeño <luis.panadero@gmail.com>
2019-03-19 19:21:50 +0100

    Update install script. Fix problems on new empty system install

--- install.sh
+++ install.sh
@@ -10,24 +10,28 @@ cd ~
 
 ln -sf ${DIR}/bashrc ~/.bashrc
 
-ln -s ${DIR}/gitconfig ~/.gitconfig
+ln -sf ${DIR}/gitconfig ~/.gitconfig
 ln -sf ${DIR}/gitconfig-common ~/.gitconfig-common
 ln -sf ${DIR}/gitconfig-personal ~/.gitconfig-personal
 ln -sf ${DIR}/gitconfig-work ~/.gitconfig-work
 
+mkdir -p ~/.config/nvim
 ln -sf ${DIR}/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ${DIR}/config/nvim/ginit.vim ~/.config/nvim/ginit.vim
 ln -sf ${DIR}/config/nvim/init.vim ~/.vimrc
 
+mkdir -p ~/.config/fish
 ln -sf ${DIR}/config/fish/config.fish ~/.config/fish/config.fish
 ln -sf ${DIR}/config/fish/fishfile ~/.config/fish/fishfile
 
-ln -sf ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.kde/share/apps/konsole/WhiteOnBlack.colorscheme
-ln -sf ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.kde/share/apps/konsole/Mio.colorscheme
-ln -sf ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.kde/share/apps/konsole/Shell.profile
-ln -sf ${DIR}/kde/share/apps/konsole/Fish.profile             ~/.kde/share/apps/konsole/Fish.profile
+#mkdir -p ~/.kde/share/apps/konsole
+#ln -sf ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.kde/share/apps/konsole/WhiteOnBlack.colorscheme
+#ln -sf ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.kde/share/apps/konsole/Mio.colorscheme
+#ln -sf ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.kde/share/apps/konsole/Shell.profile
+#ln -sf ${DIR}/kde/share/apps/konsole/Fish.profile             ~/.kde/share/apps/konsole/Fish.profile
 
 
+mkdir -p ~/.local/share/konsole
 ln -sf ${DIR}/kde/share/apps/konsole/WhiteOnBlack.colorscheme ~/.local/share/konsole/WhiteOnBlack.colorscheme
 ln -sf ${DIR}/kde/share/apps/konsole/Mio.colorscheme          ~/.local/share/konsole/Mio.colorscheme
 ln -sf ${DIR}/kde/share/apps/konsole/Shell.profile            ~/.local/share/konsole/Shell.profile
