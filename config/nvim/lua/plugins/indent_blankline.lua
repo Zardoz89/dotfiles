0ea8e7b679f8e7c9a49fb5698973b9dfafa4d510
Luis Panadero Guardeño <luis.panadero@gmail.com>
2026-01-19 22:58:07 +0100

    config indent-blankline

--- config/nvim/lua/plugins/indent_blankline.lua
+++ config/nvim/lua/plugins/indent_blankline.lua
@@ -1,7 +1,27 @@
+local highlight = {
+  "CursorColumn",
+  "Whitespace",
+}
+
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+  "lukas-reineke/indent-blankline.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  --main = "ibl",
+  -----@module "ibl"
+  -----@type ibl.config
+  --opts = {},
+  config = function()
+    require("ibl").setup {
+      --indent = { highlight = highlight, char = "" },
+      --whitespace = { highlight = { "Whitespace", "NonText" } },
+      indent = { char = "▏" },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = { enabled = true },
+    }
+  end,
 }
