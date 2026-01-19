6c8bd9b71158fdbcb08c1e318114e6c0a937301d
Luis Panadero Guardeño <luis.panadero@gmail.com>
2026-01-19 22:58:25 +0100

    Added extra plugins

---
+++ config/nvim/lua/plugins/hbac.lua
@@ -0,0 +1,5 @@
+-- Automagically close the unedited buffers in your bufferlist when it becomes too long.
+return {
+  'axkirillov/hbac.nvim',
+  config = true,
+}
---
+++ config/nvim/lua/plugins/treesiter-context.lua
@@ -0,0 +1,9 @@
+-- Show conxtext for functions, etc
+
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = { 
+    "nvim-treesitter/nvim-treesitter",
+  }
+}
+
